94e30d4afd6f4ca90581477eceb878258e580d92
shuvomdhar <shuvomdhar8@gmail.com>
2025-08-24 16:34:58 +0530

    Added Django docs

---
+++ Django/chaiaurDjango/chai/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .models import ChaiVarity, Store
+from django.shortcuts import get_object_or_404
+from .forms import ChaiVarityForm
+
+# Create your views here.
+def all_chai(request):
+    chais = ChaiVarity.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais': chais})
+
+def chai_detail(request, chai_id):
+    chai = get_object_or_404(ChaiVarity, pk=chai_id)
+    return render(request, 'chai/chai_detail.html', {'chai':chai})
+
+def chai_store_view(request):
+    stores = None
+    if request.method == 'POST':
+        form = ChaiVarityForm(request.POST)
+        if form.is_valid():
+            chai_varity = form.cleaned_data['chai_varity']
+            stores = Store.objects.filter(chai_varities=chai_varity)
+    else:
+        form = ChaiVarityForm()
+    return render(request, 'chai/chai_stores.html', {'stores': stores, 'form': form})
